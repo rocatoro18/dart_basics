f6b47d53c9ffa49aede53539e72a14727d007eac
Carlos Torres <59813315+rocatoro18@users.noreply.github.com>
2023-01-09 10:25:29 -0700

    Strings and numbers

---
+++ 02-strings_numbers.dart
@@ -0,0 +1,14 @@
+void main() {
+  // String
+  String nombre = "Roberto Carlos";
+  var apellido = "Torres";
+  final String apellido2 = "Rosas";
+
+  // Números
+  int numeroEntero = 18;
+  double numeroDecimal = 69.420;
+
+  print('$nombre $apellido $apellido2');
+
+  print('$numeroEntero $numeroDecimal');
+}
